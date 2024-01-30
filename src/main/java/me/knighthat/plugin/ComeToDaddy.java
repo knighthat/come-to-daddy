e2ed5e9144a3a29e5c99d1c95a6c4e6c95d5a523
KnightHat <knightchanelgaming@gmail.com>
2024-01-29 21:09:35 -0600

    implemented messages.yml to plugin

--- src/main/java/me/knighthat/plugin/ComeToDaddy.java
+++ src/main/java/me/knighthat/plugin/ComeToDaddy.java
@@ -1,38 +1,46 @@
 package me.knighthat.plugin;
 
+import lombok.Getter;
 import me.knighthat.plugin.data.DataHandler;
 import me.knighthat.plugin.event.EventListener;
+import me.knighthat.plugin.file.MessageFile;
 import me.knighthat.plugin.item.MagnetItem;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
+@Getter
 public class ComeToDaddy extends JavaPlugin {
 
+    @NotNull
+    private final MessageFile messages;
 
     public ComeToDaddy() {
-        DataHandler.KEY = new NamespacedKey( this, "ComeToDaddy" );
+        DataHandler.KEY = new NamespacedKey(this, "ComeToDaddy");
+
+        this.messages = new MessageFile(this);
     }
 
     @Override
     public void onEnable() {
         // Register event listener
-        getServer().getPluginManager().registerEvents( new EventListener( this ), this );
+        getServer().getPluginManager().registerEvents(new EventListener(this), this);
 
         // Register command
         //getCommand( "ctd" ).setExecutor( new CommandManager( this ) );
 
-        ShapedRecipe recipe = new ShapedRecipe( DataHandler.KEY, new MagnetItem() );
+        ShapedRecipe recipe = new ShapedRecipe(DataHandler.KEY, new MagnetItem());
         recipe.shape(
                 "LII",
                 "  I",
                 "RII"
         );
-        recipe.setIngredient( 'L', Material.LAPIS_LAZULI );
-        recipe.setIngredient( 'I', Material.IRON_INGOT );
-        recipe.setIngredient( 'R', Material.REDSTONE );
+        recipe.setIngredient('L', Material.LAPIS_LAZULI);
+        recipe.setIngredient('I', Material.IRON_INGOT);
+        recipe.setIngredient('R', Material.REDSTONE);
 
-        getServer().addRecipe( recipe );
+        getServer().addRecipe(recipe);
     }
 }

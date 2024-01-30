a186a6085a5f81bf0e6bb066baf8abfe660f59f8
KnightHat <knightchanelgaming@gmail.com>
2024-01-29 21:09:11 -0600

    added messages.yml file

---
+++ src/main/java/me/knighthat/plugin/file/MessageFile.java
@@ -0,0 +1,34 @@
+package me.knighthat.plugin.file;
+
+import lombok.Getter;
+import me.knighthat.plugin.ComeToDaddy;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class MessageFile extends PluginFile {
+
+    @NotNull
+    private static Component prefix;
+
+    public MessageFile(@NotNull ComeToDaddy plugin) {
+        super(plugin, "messages");
+    }
+
+    private @NotNull TextComponent color(@NotNull String s) {
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(s);
+    }
+
+    public @NotNull Component message(@NotNull String path) {
+        String msgRaw = get().getString(path, "");
+        return prefix.append(color(msgRaw));
+    }
+
+    @Override
+    public void reload() {
+        super.reload();
+        prefix = color(get().getString("prefix", ""));
+    }
+}

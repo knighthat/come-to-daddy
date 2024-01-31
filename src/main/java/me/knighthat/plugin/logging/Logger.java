31516338f24effc3da708d7ecdd8cb7ae26989aa
KnightHat <knightchanelgaming@gmail.com>
2024-01-31 16:09:11 -0600

    introduced a logging system

---
+++ src/main/java/me/knighthat/plugin/logging/Logger.java
@@ -0,0 +1,33 @@
+package me.knighthat.plugin.logging;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.event.Level;
+
+public final class Logger {
+
+    public static org.slf4j.Logger LOGGER;
+
+    public static void log( @NotNull Level level, @NotNull String s ) {
+        switch (level) {
+            case ERROR -> LOGGER.error( s );
+            case WARN -> LOGGER.warn( s );
+            case INFO -> LOGGER.info( s );
+            case DEBUG -> LOGGER.debug( s );
+            case TRACE -> LOGGER.trace( s );
+        }
+    }
+
+    public static void warn( @NotNull String s ) { log( Level.WARN, s ); }
+
+    public static void error( @NotNull String s ) { log( Level.ERROR, s ); }
+
+    public static void exception( @NotNull String s, @NotNull Exception e ) {
+        if ( !s.isEmpty() )
+            error( s );
+        else if ( !e.getMessage().isEmpty() )
+            error( e.getMessage() );
+
+        if ( e.getCause() != null )
+            error( "Reason: " + e.getCause().getMessage() );
+    }
+}
